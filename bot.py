62162ddead988184777a10c12ec472c39216d6ed
abhiethic <168088445+abhiethic@users.noreply.github.com>
2025-09-15 16:32:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -59,6 +59,7 @@ async def op(_, m :Message):
                         InlineKeyboardButton("💬 Support", url="https://t.me/inter_bug")
                     ],[
                         InlineKeyboardButton("➕ Add me to your Chat ➕", url="https://t.me/AcceptronBot?startgroup")
+                        InlineKeyboardButton("➕ Add me to your Channel ➕", url="https://t.me/AcceptronBot?startchannel")
                     ]
                 ]
             )
@@ -101,6 +102,7 @@ async def chk(_, cb : CallbackQuery):
                         InlineKeyboardButton("💬 Support", url="https://t.me/inter_bug")
                     ],[
                         InlineKeyboardButton("➕ Add me to your Chat ➕", url="https://t.me/AcceptronBot?startgroup")
+                        InlineKeyboardButton("➕ Add me to your Channel ➕", url="https://t.me/AcceptronBot?startchannel")
                     ]
                 ]
             )
