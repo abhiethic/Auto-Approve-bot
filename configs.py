63838459b9874a984a114760d480fb43dcc961ad
abhiethic <168088445+abhiethic@users.noreply.github.com>
2025-09-16 11:34:36 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,8 +4,8 @@ class Config:
     API_ID = int(getenv("API_ID", "0112234"))
     API_HASH = getenv("API_HASH", "abcdefg")
     BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
-    FSUB = getenv("FSUB", "inter_bug")
-    CHID = int(getenv("CHID", "-1001842891309"))
+    FSUB = getenv("FSUB", "AcceptronBotUpdatesg")
+    CHID = int(getenv("CHID", "-1002751994289"))
     SUDO = list(map(int, getenv("SUDO").split()))
     MONGO_URI = getenv("MONGO_URI", "")
     
